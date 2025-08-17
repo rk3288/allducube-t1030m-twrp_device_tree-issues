56ef3460cd77383d72b8e9a0a25db46d2a159050
rk3288 <m18749410674@163.com>
2025-08-17 12:03:19 +0800

    Update omni_T1030M.mk

--- omni_T1030M.mk
+++ omni_T1030M.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 
 # Inherit from T1030M device
 $(call inherit-product, device/alldocube/T1030M/device.mk)
