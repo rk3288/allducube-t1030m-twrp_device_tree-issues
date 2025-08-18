748f0b257e5942d2dbd767057a0724b38ca31098
rk3288 <m18749410674@163.com>
2025-08-18 14:58:12 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/alldocube/T1030M
+LOCAL_PATH := device/alldocube/t1030m
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
