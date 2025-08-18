0a9890fae0e0b60a35dc66fe68e03cce968382dd
rk3288 <m18749410674@163.com>
2025-08-18 15:55:48 +0800

    Update omni_T1030M.mk

--- omni_T1030M.mk
+++ omni_T1030M.mk
@@ -10,7 +10,7 @@
 # Inherit from T1030M device
 $(call inherit-product, device/alldocube/t1030m/device.mk)
 
-PRODUCT_DEVICE := T1030M
+PRODUCT_DEVICE := t1030m
 PRODUCT_NAME := omni_T1030M
 PRODUCT_BRAND := alldocube
 PRODUCT_MODEL := iPlay 50 Pro
@@ -21,4 +21,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-alldocube
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vext_k6789v1_64-user 12 SP1A.210812.016 1711613054 release-keys"
 
-BUILD_FINGERPRINT := alldocube/iPlay_50_Pro/T1030M:12/SP1A.210812.016/1711613054:user/release-keys
+BUILD_FINGERPRINT := alldocube/iPlay_50_Pro/t1030m:12/SP1A.210812.016/1711613054:user/release-keys
