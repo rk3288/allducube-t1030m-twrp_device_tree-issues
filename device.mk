68ead407cd4406a3cc03701dddf1e21155f354fb
rk3288 <m18749410674@163.com>
2025-08-23 13:37:24 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -41,4 +41,19 @@ PRODUCT_PACKAGES += \
 	mtk_plpath_utils.recovery
 	
 PRODUCT_PACKAGES_DEBUG += \
-	bootctrl	        
+	bootctrl	 
+
+
+# A/B
+PRODUCT_PACKAGES += \
+	update_engine \
+	update_engine_sideload \
+	update_verifier \
+	mtk_plpath_utils_ota \
+
+
+AB_OTA_POSTINSTALL_CONFIG += \
+	RUN_POSTINSTALL_vendor=true \
+	POSTINSTALL_PATH_vendor=bin/mtk_plpath_utils_ota \
+	FILESYSTEM_TYPE_vendor=ext4 \
+	POSTINSTALL_OPTIONAL_vendor=true
