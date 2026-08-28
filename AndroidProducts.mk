f6b7362136e6fcb28f66a2133a702d31a69986c3
rk3288 <m18749410674@163.com>
2026-08-28 23:30:43 +0800

    Replace omni_T1030M with twrp_T1030M in lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_T1030M.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_T1030M-user \
-    omni_T1030M-userdebug \
-    omni_T1030M-eng
+    twrp_T1030M-user \
+    twrp_T1030M-userdebug \
+    twrp_T1030M-eng
